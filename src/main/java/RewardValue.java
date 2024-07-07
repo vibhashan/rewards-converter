85cca6b9c897a363aca649e3c91ee766ce9bd3e9
vibhashan <ranhindavibhashana@2002>
2024-07-07 17:34:11 +0530

    Completed RewardValue class implementation and test suite implementation.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,29 +1,28 @@
 public class RewardValue {
     private double cashValue;
-    private int milesValue;
-    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
+    public static final double CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
     }
 
     public RewardValue(int milesValue) {
-        this.milesValue = milesValue;
+        this.cashValue = convertMilesToCash(milesValue);
     }
 
     public double getCashValue() {
-        return convertMilesToCash(milesValue);
+        return cashValue;
     }
 
     public int getMilesValue() {
         return convertCashToMiles(cashValue);
     }
 
-    public int convertCashToMiles(double cashValue) {
-        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
+    private int convertCashToMiles(double cashValue) {
+        return (int) (cashValue / CONVERSION_RATE);
     }
 
-    public double convertMilesToCash(int milesValue) {
-        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
+    private double convertMilesToCash(int milesValue) {
+        return milesValue * CONVERSION_RATE;
     }
 }
