727c7c07f20259a60948fb606aedca189d0d47d6
vibhashan <ranhindavibhashana@2002>
2024-07-07 16:57:01 +0530

    Modified RewardValue class implementation.

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,7 +1,7 @@
 public class RewardValue {
     private double cashValue;
     private int milesValue;
-    private static final double CASH_TO_MILES_CONVERSION_RATE = 0.0035;
+    private static final double MILES_TO_CASH_CONVERSION_RATE = 0.0035;
 
     public RewardValue(double cashValue) {
         this.cashValue = cashValue;
@@ -12,18 +12,18 @@ public class RewardValue {
     }
 
     public double getCashValue() {
-        return cashValue;
+        return convertMilesToCash(milesValue);
     }
 
     public int getMilesValue() {
-        return milesValue;
+        return convertCashToMiles(cashValue);
     }
 
-    public int convertCashToMiles() {
-        return (int) (cashValue / CASH_TO_MILES_CONVERSION_RATE);
+    public int convertCashToMiles(double cashValue) {
+        return (int) (cashValue / MILES_TO_CASH_CONVERSION_RATE);
     }
 
-    public double convertMilesToCash() {
-        return milesValue * CASH_TO_MILES_CONVERSION_RATE;
+    public double convertMilesToCash(int milesValue) {
+        return milesValue * MILES_TO_CASH_CONVERSION_RATE;
     }
 }
